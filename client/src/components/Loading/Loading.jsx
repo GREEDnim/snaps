3451053f2fb27281d74c9370eb3a476b68aaf9f9
GREEDnim <sivadhas007@gmail.com>
2023-07-24 18:10:45 +0530

    modified class names

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Loading.css'
 export default function Loading() {
   return (
-    <div className="container">
+    <div className="loading-container">
     <div className="loading-wrapper">
         <div className="loading-text">Starting the server...</div>
     </div>
